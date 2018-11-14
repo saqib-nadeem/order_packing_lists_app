c1fade4a2543769d3aed4732b7536b713099eea5
Saqib Nadeem <saqib598@gmail.com>
2018-11-14 20:27:40 +0500

    Test deploy

--- utils.py
+++ utils.py
@@ -3,48 +3,48 @@ import time
 import shutil
 import traceback
 
-from settings import options, WKHTMLTOPDF_PACKAGE_PATH, PDFS_DIRECTORY
+# from settings import options, WKHTMLTOPDF_PACKAGE_PATH, PDFS_DIRECTORY
 
-import pdfkit
+# import pdfkit
 
 
-class PdfGenerator:
+# class PdfGenerator:
 
-	def create_file(self, order_id, directory):
-		config = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_PACKAGE_PATH)	
-		output_file = '{directory}/order_{id}.pdf'.format(directory=directory, id=order_id[-1])
-		pdf = pdfkit.from_string(order_id, output_file, configuration=config, options=options)
-		return pdf
+# 	def create_file(self, order_id, directory):
+# 		config = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_PACKAGE_PATH)	
+# 		output_file = '{directory}/order_{id}.pdf'.format(directory=directory, id=order_id[-1])
+# 		pdf = pdfkit.from_string(order_id, output_file, configuration=config, options=options)
+# 		return pdf
 
-	def create_directory(self, username):
-		timestamp = int(time.time())
-		directory = "{pdfs_dir}/{username}_files_{timestamp}".format(pdfs_dir=PDFS_DIRECTORY, username=username, timestamp=timestamp)
-		try:
-		    os.makedirs(directory)
-		except:
-			traceback.print_exc()
-			raise
-			return None
-		return directory			    	
+# 	def create_directory(self, username):
+# 		timestamp = int(time.time())
+# 		directory = "{pdfs_dir}/{username}_files_{timestamp}".format(pdfs_dir=PDFS_DIRECTORY, username=username, timestamp=timestamp)
+# 		try:
+# 		    os.makedirs(directory)
+# 		except:
+# 			traceback.print_exc()
+# 			raise
+# 			return None
+# 		return directory			    	
 
-	def remove_file(self, filename):
-		try:
-			os.remove(filename)
-		except:
-			traceback.print_exc()
+# 	def remove_file(self, filename):
+# 		try:
+# 			os.remove(filename)
+# 		except:
+# 			traceback.print_exc()
 
-	def remove_directory(self, directory):
-		try:
-			shutil.rmtree(directory)
-		except:
-			traceback.print_exc() 	 
+# 	def remove_directory(self, directory):
+# 		try:
+# 			shutil.rmtree(directory)
+# 		except:
+# 			traceback.print_exc() 	 
 
-	def compress_directory(self, directory):
-		try:
-			shutil.make_archive(directory, 'zip', directory)
-			self.remove_directory(directory)		
-		except:
-			traceback.print_exc()
+# 	def compress_directory(self, directory):
+# 		try:
+# 			shutil.make_archive(directory, 'zip', directory)
+# 			self.remove_directory(directory)		
+# 		except:
+# 			traceback.print_exc()
 
 class Address:
     
